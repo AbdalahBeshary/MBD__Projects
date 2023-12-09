5d54a242ec9bee779ed7e87a3770ce3715e508de
AbdalahBeshary <146075961+AbdalahBeshary@users.noreply.github.com>
2023-12-09 21:11:17 +0200

    Add files via upload

---
+++ Pjoects/Pedal_sensor_applied_by_proteus/APP_Modes.m
@@ -0,0 +1,8 @@
+classdef (Enumeration) APP_Modes < Simulink.IntEnumType
+        enumeration 
+            normal(0)
+            Downsensor_sens_1(1)
+            Downsensor_sens_2(2)
+            Failure(3)
+        end
+end
