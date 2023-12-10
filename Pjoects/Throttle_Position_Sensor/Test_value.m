fd4bfd92ac5377671f43e55c9c2e7a2f51268d7c
AbdalahBeshary <146075961+AbdalahBeshary@users.noreply.github.com>
2023-12-10 07:30:29 +0200

    Add files via upload

---
+++ Pjoects/Throttle_Position_Sensor/Test_value.m
@@ -0,0 +1,5 @@
+t=0:1:7;
+t=t';
+Thtottle_Position_Senor_1=[2*pi,0,0.1*pi,0.6*pi,3*pi,0.4*pi,2*pi,3*pi]';
+Thtottle_Position_Senor_2=[3*pi,2*pi,0.4*pi,0.2*pi,0.1*pi,0*pi,2*pi,0]';
+Thtottle_Position_Setpoint=[2,2,2,2,2,2,2,2]';
